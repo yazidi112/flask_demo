4a0891dc0be006b449a5887f2622f80d03f120a8
yazidi112 <yazidi.imran@gmail.com>
2020-09-04 11:27:22 +0100

    add upload

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep  4 10:05:52 2020
-
-@author: imran
-"""
-
-from flask import Flask, escape, request
+import os
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run()
